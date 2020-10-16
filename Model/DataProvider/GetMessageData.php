ae12442aff4c3caf7cbeead9a2674057a012653a
huykon225 <huykon225@gmail.com>
2020-10-16 10:30:20 +0700

    update send message include attachments to exactly msg id

--- Model/DataProvider/GetMessageData.php
+++ Model/DataProvider/GetMessageData.php
@@ -1,105 +1,97 @@
 <?php
+
 namespace Simi\RMAGraphQL\Model\DataProvider;
 
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Filesystem;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory as BestSellersCollectionFactory;
-use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
-use Mirasvit\Rma\Api\Config\RmaPolicyConfigInterface;
-use Magento\Framework\Filesystem;
-use Magento\Framework\App\Filesystem\DirectoryList;
 
 
+class GetMessageData {
+	protected $orderCollectionFactory;
+	protected $itemModelFactory;
+	protected $messageFactory;
+	protected $attachmentFactory;
+	protected $fileSystem;
+	protected $attachmentRepository;
+	protected $storeManager;
 
+	public function __construct(
+		\Mirasvit\Rma\Model\ResourceModel\Item\CollectionFactory $itemModelFactory,
+		\Mirasvit\Rma\Model\ResourceModel\Message\CollectionFactory $messageFactory,
+		\Mirasvit\Rma\Model\ResourceModel\Attachment\CollectionFactory $attachmentFactory,
+		Filesystem $fileSystem,
+		\Magento\Store\Model\StoreManagerInterface $storeManager,
+		\Mirasvit\Rma\Api\Repository\AttachmentRepositoryInterface $attachmentRepository
+	) {
+		$this->itemModelFactory     = $itemModelFactory;
+		$this->messageFactory       = $messageFactory;
+		$this->attachmentFactory    = $attachmentFactory;
+		$this->fileSystem           = $fileSystem;
+		$this->attachmentRepository = $attachmentRepository;
+		$this->storeManager         = $storeManager;
+	}
 
-class GetMessageData
-{
-    protected $orderCollectionFactory;
-    protected $itemModelFactory;
-    protected $messageFactory;
-    protected $attachmentFactory;
-    protected $fileSystem;
-    protected $attachmentRepository;
-    protected $storeManager;
+	//@param: the array of the id of orders to return
+	public function dataArray( $rma_id ) {
+		try {
+			$storeBaseUrl   = $this->storeManager->getStore()->getBaseUrl();
+			$rmaDataMessage = [];
+			$messages       = $this->messageFactory->create()->addFieldToFilter( 'is_visible_in_frontend', '1' )->addFieldToFilter( 'rma_id', $rma_id )->getData();
 
-    public function __construct(
-        \Mirasvit\Rma\Model\ResourceModel\Item\CollectionFactory $itemModelFactory,
-        \Mirasvit\Rma\Model\ResourceModel\Message\CollectionFactory $messageFactory,
-        \Mirasvit\Rma\Model\ResourceModel\Attachment\CollectionFactory $attachmentFactory,
-        Filesystem $fileSystem,
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Mirasvit\Rma\Api\Repository\AttachmentRepositoryInterface $attachmentRepository
-    ) {
-        $this->itemModelFactory = $itemModelFactory;
-        $this->messageFactory = $messageFactory;
-        $this->attachmentFactory = $attachmentFactory;
-        $this->fileSystem = $fileSystem;
-        $this->attachmentRepository      = $attachmentRepository;
-        $this->storeManager              = $storeManager;
-    }
-    //@param: the array of the id of orders to return
-    public function dataArray($rma_id)
-    {
-       try
-        {   
-        $storeBaseUrl   = $this->storeManager->getStore()->getBaseUrl();
-        $rmaDataMessage = [];
-        $messages = $this->messageFactory->create()->addFieldToFilter( 'is_visible_in_frontend', '1' )->addFieldToFilter( 'rma_id', $rma_id )->getData();
-        foreach ( $messages as $message ) 
-        {
-                //search whether this message have attachment or not
-            $attachmentsData = $this->attachmentFactory->create()->addFieldToFilter( 'item_id', $message['message_id'] );
-            $attachments     = [];
-            if ( $attachmentsData ) 
-            {
-                    // die('123');
-                $mediaPath     = $this->fileSystem->getDirectoryRead( DirectoryList::MEDIA )->getAbsolutePath();
-                $originalPath  = 'Simicustomize/graphql/';
-                $mediaFullPath = $mediaPath . $originalPath;
+			foreach ( $messages as $message ) {
+				//search whether this message have attachment or not
+				$attachmentsData = $this->attachmentFactory->create()->addFieldToFilter( 'item_id', $message['message_id'] );
+				$attachments     = [];
+				if ( $attachmentsData ) {
+					$mediaPath     = $this->fileSystem->getDirectoryRead( DirectoryList::MEDIA )->getAbsolutePath();
+					$originalPath  = 'Simicustomize/graphql/';
+					$mediaFullPath = $mediaPath . $originalPath;
 
-                foreach ( $attachmentsData as $attachmentData ) 
-                {
-                    $uid                     = $attachmentData['uid'];
-                    $attachment              = $this->attachmentRepository->getByUid( $uid );
-                    $fileName                = $attachment->getName();
-                    $fileType                = $attachment->getType();
-                    $attachmentControllerUrl = $storeBaseUrl . 'returns/attachment/download/uid/' . $uid;
+					foreach ( $attachmentsData as $attachmentData ) {
+						$uid                     = $attachmentData['uid'];
+						$attachment              = $this->attachmentRepository->getByUid( $uid );
+						$fileName                = $attachment->getName();
+						$fileType                = $attachment->getType();
+						$attachmentControllerUrl = $storeBaseUrl . 'returns/attachment/download/uid/' . $uid;
 
-                    $attachment = [
-                        'name'       => $fileName,
-                        'type'       => $fileType,
-                        'full_path'  => $mediaFullPath . $fileName,
-                        'quote_path' => $originalPath . $fileName,
-                        'order_path' => $originalPath . $fileName,
-                        'link'       => $attachmentControllerUrl
-                            // 'secret_key' => substr( md5( file_get_contents( $mediaFullPath . $fileName ) ), 0, 20 )
-                    ];
-                    array_push( $attachments, $attachment );
-                }
-            }
-             // die(var_dump($message['customer_id']));
-            $messageObject = [
-                'message_id'    => $message['message_id'],
-                'user_id'       => $message['user_id'],
-                'customer_name' => $message['customer_name'],
-                'customer_id'   => $message['customer_id'],
-                'content'       => $message['text'],
-                'is_read'       => $message['is_read'],
-                'created_at'    => $message['created_at'],
-                'updated_at'    => $message['updated_at'],
-                'items'         => $attachments
-            ];
-            array_push( $rmaDataMessage, $messageObject );
-        }
-        return $rmaDataMessage;
-        }
-    catch (\Magento\Framework\Exception\LocalizedException $e) 
-    {
-        throw new \Magento\Framework\Exception\LocalizedException(__('no rma has this id'));
-    }
-}
+						$attachment    = [
+							'name'       => $fileName,
+							'type'       => $fileType,
+							'full_path'  => $mediaFullPath . $fileName,
+							'quote_path' => $originalPath . $fileName,
+							'order_path' => $originalPath . $fileName,
+							'link'       => $attachmentControllerUrl
+							// 'secret_key' => substr( md5( file_get_contents( $mediaFullPath . $fileName ) ), 0, 20 )
+						];
+						$attachments[] = $attachment;
+					}
+				}
+
+				$messageObject    = [
+					'message_id'    => $message['message_id'],
+					'user_id'       => $message['user_id'],
+					'customer_name' => $message['customer_name'],
+					'customer_id'   => $message['customer_id'],
+					'content'       => $message['text'],
+					'is_read'       => $message['is_read'],
+					'created_at'    => $message['created_at'],
+					'updated_at'    => $message['updated_at'],
+					'items'         => $attachments
+				];
+				$rmaDataMessage[] = $messageObject;
+			}
+
+			return $rmaDataMessage;
+		}
+		catch ( \Magento\Framework\Exception\LocalizedException $e ) {
+			throw new \Magento\Framework\Exception\LocalizedException( __( 'no rma has this id' ) );
+		}
+	}
 }
--- Model/Resolver/SendMessage.php
+++ Model/Resolver/SendMessage.php
@@ -1,271 +1,253 @@
 <?php
+
 namespace Simi\RMAGraphQL\Model\Resolver;
 
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Filesystem;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory as BestSellersCollectionFactory;
-use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
-use Mirasvit\Rma\Api\Config\RmaPolicyConfigInterface;
-use Magento\Framework\Filesystem;
-use Magento\Framework\App\Filesystem\DirectoryList;
-use Magento\Framework\Exception\LocalizedException;
 
-class SendMessage implements ResolverInterface 
-{
+class SendMessage implements ResolverInterface {
 
-    protected $messageDataProvider;
-    protected $rmaDataProvider;
-    protected $rmaCollectionFactory;
-    protected $customerStrategy;
-    protected $guestStrategy;
-    protected $messageRepository;
-    protected $rmaRepository;
-    protected $filterFactory;
-    protected $urlModel;
-    protected $rmaMail;
-    private $fileSystem;
-    public $attachmentRepository;
-    protected $config;
+	protected $messageDataProvider;
+	protected $rmaDataProvider;
+	protected $rmaCollectionFactory;
+	protected $customerStrategy;
+	protected $guestStrategy;
+	protected $messageRepository;
+	protected $rmaRepository;
+	protected $filterFactory;
+	protected $urlModel;
+	protected $rmaMail;
+	private $fileSystem;
+	public $attachmentRepository;
+	protected $config;
 
-    public function __construct(
-        \Simi\RMAGraphQL\Model\DataProvider\RmaDataArray $rmaDataProvider,
-        \Simi\RMAGraphQL\Model\DataProvider\GetMessageData $messageDataProvider,
-        \Mirasvit\Rma\Helper\Controller\Rma\CustomerStrategy $customerStrategy,
-        GuestStrategy $guestStrategy,
-        \Mirasvit\Rma\Model\ResourceModel\Rma\CollectionFactory $rmaCollectionFactory,
-        \Mirasvit\Rma\Api\Repository\MessageRepositoryInterface $messageRepository,
-        \Mirasvit\Rma\Api\Repository\RmaRepositoryInterface $rmaRepository,
-        \Magento\Email\Model\Template\FilterFactory $filterFactory,
-        \Magento\Framework\UrlInterface $urlModel,
-        \Mirasvit\Rma\Helper\Mail $rmaMail,
-        Filesystem $fileSystem,
-        \Mirasvit\Rma\Api\Repository\AttachmentRepositoryInterface $attachmentRepository,
-        \Mirasvit\Rma\Api\Config\AttachmentConfigInterface $config
+	public function __construct(
+		\Simi\RMAGraphQL\Model\DataProvider\RmaDataArray $rmaDataProvider,
+		\Simi\RMAGraphQL\Model\DataProvider\GetMessageData $messageDataProvider,
+		\Mirasvit\Rma\Helper\Controller\Rma\CustomerStrategy $customerStrategy,
+		GuestStrategy $guestStrategy,
+		\Mirasvit\Rma\Model\ResourceModel\Rma\CollectionFactory $rmaCollectionFactory,
+		\Mirasvit\Rma\Api\Repository\MessageRepositoryInterface $messageRepository,
+		\Mirasvit\Rma\Api\Repository\RmaRepositoryInterface $rmaRepository,
+		\Magento\Email\Model\Template\FilterFactory $filterFactory,
+		\Magento\Framework\UrlInterface $urlModel,
+		\Mirasvit\Rma\Helper\Mail $rmaMail,
+		Filesystem $fileSystem,
+		\Mirasvit\Rma\Api\Repository\AttachmentRepositoryInterface $attachmentRepository,
+		\Mirasvit\Rma\Api\Config\AttachmentConfigInterface $config
 
-    ) {
-        $this->rmaDataProvider = $rmaDataProvider;
-        $this->messageDataProvider = $messageDataProvider;
-        $this->rmaCollectionFactory = $rmaCollectionFactory;
-        $this->customerStrategy = $customerStrategy;
-        $this->guestStrategy = $guestStrategy;
-        $this->messageRepository     = $messageRepository;
-        $this->rmaRepository         = $rmaRepository;
-        $this->filterFactory         = $filterFactory;
-        $this->urlModel              = $urlModel;
-        $this->rmaMail               = $rmaMail;
-        $this->fileSystem = $fileSystem;
-        $this->attachmentRepository = $attachmentRepository;
-        $this->config                = $config;
-    }
+	) {
+		$this->rmaDataProvider      = $rmaDataProvider;
+		$this->messageDataProvider  = $messageDataProvider;
+		$this->rmaCollectionFactory = $rmaCollectionFactory;
+		$this->customerStrategy     = $customerStrategy;
+		$this->guestStrategy        = $guestStrategy;
+		$this->messageRepository    = $messageRepository;
+		$this->rmaRepository        = $rmaRepository;
+		$this->filterFactory        = $filterFactory;
+		$this->urlModel             = $urlModel;
+		$this->rmaMail              = $rmaMail;
+		$this->fileSystem           = $fileSystem;
+		$this->attachmentRepository = $attachmentRepository;
+		$this->config               = $config;
+	}
 
-    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
-    {
+	public function resolve( Field $field, $context, ResolveInfo $info, array $value = null, array $args = null ) {
+		if ( empty( $args['guest_id'] ) ) {
+			throw new GraphQlInputException( __( 'You must specify your "RMA Id".' ) );
+		}
 
-        $rma = $this->getRma($args['guest_id']);
-        $params = [
-            'isNotifyAdmin' => 1,
-            'isNotified'    => 0,
-        ];
-        $message = $args['content'];
-        $performer = $this->getPerfomer($context, $this->getOrderId($args['guest_id']));
-        
-        $this->addMessage(
-            $performer,
-            $rma,
-            $message,
-            $params
-        );
-        //return the messages of this rma
-        
-        if(isset($args['input'])){
-            {
-                //handling exception
-                if ( empty( $args['input'] ) || ! is_array( $args['input'] ) || ! count( $args['input'] ) ) {
-                    throw new GraphQlInputException( __( 'You must specify your input.' ) );
-                }
-                foreach($args['input'] as $input){
-                    if ( empty( $input['name'] ) ) {
-                        throw new GraphQlInputException( __( 'You must specify your "file name".' ) );
-                    }
+		try {
+			$rma       = $this->getRma( $args['guest_id'] );
+			$params    = [
+				'isNotifyAdmin' => 1,
+				'isNotified'    => 0,
+			];
+			$message   = $args['content'];
+			$performer = $this->getPerfomer( $context, $this->getOrderId( $args['guest_id'] ) );
 
-                    if ( empty( $input['base64_encoded_file'] ) ) {
-                        throw new GraphQlInputException( __( 'You must specify your "file".' ) );
-                    }
-                }
-                
-                $mediaPath     = $this->fileSystem->getDirectoryRead( DirectoryList::MEDIA )->getAbsolutePath();
-                $originalPath  = 'Simicustomize/graphql/';
-                $mediaFullPath = $mediaPath . $originalPath;
-                if ( ! file_exists( $mediaFullPath ) ) {
-                    mkdir( $mediaFullPath, 0775, true );
-                }
+			$this->addMessage(
+				$performer,
+				$rma,
+				$message,
+				$params
+			);
 
-                $arrayReturn = [ 'items' => null ];
+			//return the messages of this rma
+			$lastMessageCollection = $this->messageRepository->create()->getCollection()->getFirstItem();
 
-                $fileSizeLimit = (float)$this->config->getFileSizeLimit() * 1024 * 1024;
-                $allowedFileExtensions = $this->config->getFileAllowedExtensions();
-                $error = false;
-                foreach ( $args['input'] as $input ) {
-                    $fileType      = isset( $input['type'] ) ? $input['type'] : '';
-                    $fileName        = rand() . time() . '_' . $input['name'];
-                    $ext  = pathinfo($fileName, PATHINFO_EXTENSION);
-                    if (count($allowedFileExtensions) && !in_array(strtolower($ext), $allowedFileExtensions)) {
-                        $error = true;
-                        continue;
-                    }
+			if ( isset( $args['input'] ) ) {
 
-                    if ($fileSizeLimit && $size > $fileSizeLimit) {
-                        $error = true;
-                        continue;
-                    }
-                    
+				//handling exception
+				if ( empty( $args['input'] ) || ! is_array( $args['input'] ) || ! count( $args['input'] ) ) {
+					throw new GraphQlInputException( __( 'You must specify your input.' ) );
+				}
 
-                    $base64FileArray = explode( ',', $input['base64_encoded_file'] );
+				$mediaPath     = $this->fileSystem->getDirectoryRead( DirectoryList::MEDIA )->getAbsolutePath();
+				$originalPath  = 'Simicustomize/graphql/';
+				$mediaFullPath = $mediaPath . $originalPath;
+				if ( ! file_exists( $mediaFullPath ) ) {
+					mkdir( $mediaFullPath, 0775, true );
+				}
 
-                    $fileContent = base64_decode($base64FileArray[0]);
-                // die(var_dump(get_class_methods($fileBlob)));
-                    $savedFile   = fopen( $mediaFullPath . $fileName, "wb" );
-                    fwrite( $savedFile, $fileContent );
-                    fclose( $savedFile );
-                    $arrayReturn['items'][] = [
-                        'name'       => $fileName,
-                        'type'       => $fileType,
-                        'full_path'  => $mediaFullPath . $fileName,
-                        'quote_path' => $originalPath . $fileName,
-                        'order_path' => $originalPath . $fileName,
-                        'secret_key' => substr( md5( file_get_contents( $mediaFullPath . $fileName ) ), 0, 20 )
-                    ];
-                }
-                if(!$error){
-                    $this->_saveFile("message", $this->getLatestId($args['guest_id']), $fileName, $fileContent, $fileType, $input['size']);
-                }
-                else{
+				$fileSizeLimit         = (float) $this->config->getFileSizeLimit() * 1024 * 1024;
+				$allowedFileExtensions = $this->config->getFileAllowedExtensions();
+				foreach ( $args['input'] as $inputItem ) {
+					if ( empty( $inputItem['name'] ) ) {
+						throw new GraphQlInputException( __( 'You must specify your "file name".' ) );
+					}
 
-                    throw new \Magento\Framework\Exception\LocalizedException(__('Error. Uploaded file does not match requirements.'));
-                    
-                }
-            
-            }
-        }
-        $rmaIdReturn = $this->getThisRmaData($args['guest_id'])['rma_id'];
-        return $this->getMessageData($rmaIdReturn);
-    } 
-    private function getMessageData($rmaId){
-        return $this->messageDataProvider->dataArray($rmaId);
-    }
+					if ( empty( $inputItem['base64_encoded_file'] ) ) {
+						throw new GraphQlInputException( __( 'You must specify your "file".' ) );
+					}
 
-    private function getThisRmaData($guestId){
-        $collection = $this->rmaCollectionFactory->create()->addFieldToFilter('guest_id', $guestId)->getItems();
-        $dataArrays = $this->rmaDataProvider->dataArray($collection);
-        foreach($dataArrays as $dataArray){
-            if($dataArray['guest_id'] == $guestId){
-                return $dataArray;
-            }
-        }
-    }
+					$fileType = isset( $inputItem['type'] ) ? $inputItem['type'] : '';
+					$fileName = rand() . time() . '_' . $inputItem['name'];
+					$ext      = pathinfo( $fileName, PATHINFO_EXTENSION );
+					if ( count( $allowedFileExtensions ) && ! in_array( strtolower( $ext ), $allowedFileExtensions ) ) {
+						continue;
+					}
 
-    private function getLatestId($guestId){
-        $arrays = $this->getThisRmaData($guestId)['message'];
-        $lastmessage =  end($arrays);
-        return $lastmessage['message_id'];
-    }
+					if ( $fileSizeLimit && $inputItem['size'] > $fileSizeLimit ) {
+						continue;
+					}
 
-    private function getOrderId($guestId){
-        return $this->getThisRmaData($guestId)['order_id'];
-    }
+					$base64FileArray = explode( ',', $inputItem['base64_encoded_file'] );
 
-    private function getRma($incrementId){
-        $model = $this->rmaCollectionFactory->create()->getItems();
+					$fileContent = base64_decode( $base64FileArray[0] );
 
-        $count = 0;
-        foreach($model as $dataArray){
-            if($dataArray['guest_id'] == $incrementId){
-                $rma = $dataArray;
-                $count++;
-                return $rma;
-            }
-        }
-        if($count == 0){
-            throw new \Magento\Framework\Exception\LocalizedException(__('No Rma Found'));
-        }
-    }
-    private function getPerfomer($context, $orderId){
-        try {
-                $this->guestStrategy->setOrderId($orderId);
-                return $this->guestStrategy->getPerformer();            
-        } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-            return $this->noAccessStrategy->getPerformer();
-        }
-    }
+					$savedFile = fopen( $mediaFullPath . $fileName, "wb" );
+					fwrite( $savedFile, $fileContent );
+					fclose( $savedFile );
 
-    public function addMessage(
-        \Mirasvit\Rma\Api\Service\Performer\PerformerInterface $performer,
-        \Mirasvit\Rma\Api\Data\RmaInterface $rma,
-        $messageText,
-        $params = []
-    ){
-        if (!$messageText && ! $this->attachmentManagement->hasAttachments()) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('Please, enter a message'), null, 400);
-        }
-        /** @var MessageInterface $message */
-        $message = $this->messageRepository->create();
-        $message
-        ->setRmaId($rma->getId())
-        ->setText($this->processVariables($messageText, $rma));
-        // die('1234');
-        if (isset($params['statusChanged']) && $params['statusChanged']) {
-            $message->setStatusId($rma->getStatusId());
-        }
+					$this->_saveFile( "message", $lastMessageCollection->getId(), $fileName, $fileContent, $fileType, $inputItem['size'] );
+				}
+			}
 
-        $performer->setMessageAttributesBeforeAdd($message, $params);
-        $this->messageRepository->save($message);
+			return $this->getMessageData( $lastMessageCollection->getRmaId() );
+		}
+		catch ( \Exception $e ) {
+			throw new GraphQlNoSuchEntityException( __( 'Error. Uploaded file does not match requirements.' ) );
+		}
+	}
 
-        $rma->setLastReplyName($performer->getName())
-        ->setIsAdminRead($performer instanceof UserStrategy);
+	private function getMessageData( $rmaId ) {
+		return $this->messageDataProvider->dataArray( $rmaId );
+	}
 
-        $this->rmaRepository->save($rma);
+	private function getThisRmaData( $guestId ) {
+		$collection = $this->rmaCollectionFactory->create()->addFieldToFilter( 'guest_id', $guestId )->getItems();
+		$dataArrays = $this->rmaDataProvider->dataArray( $collection );
+		foreach ( $dataArrays as $dataArray ) {
+			if ( $dataArray['guest_id'] == $guestId ) {
+				return $dataArray;
+			}
+		}
+	}
 
-    }
 
-    private function processVariables($text, $rma)
-    {
-        $templateFilter = $this->filterFactory->create();
-        $templateFilter->setUseAbsoluteLinks(true)
-        ->setStoreId($rma->getStoreId())
-        ->setUrlModel($this->urlModel)
-        ->setPlainTemplateMode(true)
-        ->setVariables($this->rmaMail->getEmailVariables($rma))
-        ;
+	private function getOrderId( $guestId ) {
+		return $this->getThisRmaData( $guestId )['order_id'];
+	}
 
-        return $templateFilter->filter($text);
-    }
+	private function getRma( $incrementId ) {
+		$model = $this->rmaCollectionFactory->create()->getItems();
 
-    public function _saveFile($itemType, $itemId, $name, $body, $fileType, $size, $isReplace = false)
-    {
-        /** @var \Mirasvit\Rma\Model\Attachment $attachment */
-        $attachment = false;
-        if ($isReplace) {
-            $attachment = $this->getAttachment($itemType, $itemId);
-        }
+		$count = 0;
+		foreach ( $model as $dataArray ) {
+			if ( $dataArray['guest_id'] == $incrementId ) {
+				$rma = $dataArray;
+				$count ++;
 
-        if (!$attachment) {
-            $attachment = $this->attachmentRepository->create();
-        }
-    set_error_handler(function() { /* ignore errors */ });
-        //@tofix - need to check for max upload size and alert error
+				return $rma;
+			}
+		}
+		if ( $count == 0 ) {
+			throw new \Magento\Framework\Exception\LocalizedException( __( 'No Rma Found' ) );
+		}
+	}
 
-    $attachment
-    ->setItemType($itemType)
-    ->setItemId($itemId)
-    ->setName($name)
-    ->setSize($size)
-    ->setBody($body)
-    ->setType($fileType)
-    ->save();
-}
+	private function getPerfomer( $context, $orderId ) {
+		try {
+			$this->guestStrategy->setOrderId( $orderId );
+
+			return $this->guestStrategy->getPerformer();
+		}
+		catch ( \Magento\Framework\Exception\NoSuchEntityException $e ) {
+			return $this->noAccessStrategy->getPerformer();
+		}
+	}
+
+	public function addMessage(
+		\Mirasvit\Rma\Api\Service\Performer\PerformerInterface $performer,
+		\Mirasvit\Rma\Api\Data\RmaInterface $rma,
+		$messageText,
+		$params = []
+	) {
+		if ( ! $messageText && ! $this->attachmentManagement->hasAttachments() ) {
+			throw new \Magento\Framework\Exception\LocalizedException( __( 'Please, enter a message' ), null, 400 );
+		}
+		/** @var MessageInterface $message */
+		$message = $this->messageRepository->create();
+		$message
+			->setRmaId( $rma->getId() )
+			->setText( $this->processVariables( $messageText, $rma ) );
+		// die('1234');
+		if ( isset( $params['statusChanged'] ) && $params['statusChanged'] ) {
+			$message->setStatusId( $rma->getStatusId() );
+		}
+
+		$performer->setMessageAttributesBeforeAdd( $message, $params );
+		$this->messageRepository->save( $message );
+
+		$rma->setLastReplyName( $performer->getName() )
+		    ->setIsAdminRead( $performer instanceof UserStrategy );
+
+		$this->rmaRepository->save( $rma );
+
+	}
+
+	private function processVariables( $text, $rma ) {
+		$templateFilter = $this->filterFactory->create();
+		$templateFilter->setUseAbsoluteLinks( true )
+		               ->setStoreId( $rma->getStoreId() )
+		               ->setUrlModel( $this->urlModel )
+		               ->setPlainTemplateMode( true )
+		               ->setVariables( $this->rmaMail->getEmailVariables( $rma ) );
+
+		return $templateFilter->filter( $text );
+	}
+
+	public function _saveFile( $itemType, $itemId, $name, $body, $fileType, $size, $isReplace = false ) {
+		/** @var \Mirasvit\Rma\Model\Attachment $attachment */
+		$attachment = false;
+		if ( $isReplace ) {
+			$attachment = $this->getAttachment( $itemType, $itemId );
+		}
+
+		if ( ! $attachment ) {
+			$attachment = $this->attachmentRepository->create();
+		}
+		set_error_handler( function () { /* ignore errors */
+		} );
+		//@tofix - need to check for max upload size and alert error
+
+		$attachment
+			->setItemType( $itemType )
+			->setItemId( $itemId )
+			->setName( $name )
+			->setSize( $size )
+			->setBody( $body )
+			->setType( $fileType )
+			->save();
+	}
 
 }
